65ab5b2fde0b899fe1d88d0d7e6e755766a64c44
jayy734 <angelsoenyunt@gmail.com>
2024-07-27 10:00:59 +0300

    added a couple of unittest to test Maze class's method

--- maze.py
+++ maze.py
@@ -1,10 +1,11 @@
 from graphics import *
 from cell import *
+import random
 import time
 
 class Maze:
     def __init__(self, x1, y1, num_rows, num_cols, cell_size_x,
-                 cell_size_y, win):
+                 cell_size_y, win = None):
         
         self._cells = []
         self._x1 = x1
